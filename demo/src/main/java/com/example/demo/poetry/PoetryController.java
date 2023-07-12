c0162cf32d1a5651631a9705929df13f1107316b
jurevito <jure.vito@gmail.com>
2023-07-12 13:18:15 +0200

    Add logging to PoetryController

--- demo/src/main/java/com/example/demo/poetry/PoetryController.java
+++ demo/src/main/java/com/example/demo/poetry/PoetryController.java
@@ -1,5 +1,7 @@
 package com.example.demo.poetry;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,7 @@ import java.util.List;
 public class PoetryController {
 
     private final PoetryClient poetryClient;
+    private final Logger logger = LoggerFactory.getLogger(PoetryController.class.getName());
 
     public PoetryController(PoetryClient poetryClient) {
         this.poetryClient = poetryClient;
@@ -20,13 +23,14 @@ public class PoetryController {
 
     @GetMapping("poets")
     public ResponseEntity<List<String>> getPoets() {
+        logger.info("Retrieving all available poets.");
         List<String> poets = poetryClient.getPoets().authors();
         return ResponseEntity.ok(poets);
     }
 
     @GetMapping("poems")
-    public ResponseEntity<List<PoemResponseDto>> getPoets(@RequestParam String poet) {
-        System.out.println(poet);
+    public ResponseEntity<List<PoemResponseDto>> getPoems(@RequestParam String poet) {
+        logger.info(String.format("Retrieving poems of %s.", poet));
         List<PoemResponseDto> poems = poetryClient.getPoems(poet);
         return ResponseEntity.ok(poems);
     }
