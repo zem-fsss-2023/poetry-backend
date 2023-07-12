295962ed42562bebb13db3d2801e4d5b87c66c9b
Katarinalg <kl1786@student.uni-lj.si>
2023-07-12 14:47:55 +0200

    Add PoetryClient timer and counter, properties

--- demo/src/main/java/com/example/demo/poetry/PoetryClientImpl.java
+++ demo/src/main/java/com/example/demo/poetry/PoetryClientImpl.java
@@ -3,6 +3,9 @@ package com.example.demo.poetry;
 import feign.Feign;
 import feign.gson.GsonDecoder;
 import feign.gson.GsonEncoder;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,24 +13,31 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class PoetryClientImpl implements PoetryClient{
 
     private final PoetryApi poetryApi;
     private final Logger logger = LoggerFactory.getLogger(PoetryClient.class.getName());
+    private final Counter counter;
+    private final Timer timer;
 
     @Autowired
-    public PoetryClientImpl(@Value("${poetry.db.api.url}") final String url) {
+    public PoetryClientImpl(@Value("${poetry.db.api.url}") final String url, MeterRegistry meterRegistry) {
         this.poetryApi = Feign.builder()
                 .decoder(new GsonDecoder())
                 .encoder(new GsonEncoder())
                 .target(PoetryApi.class, url);
+        counter = meterRegistry.counter("getPoetsCounter", "");
+        timer = meterRegistry.timer("getPoetsExcectionTimer", "");
     }
 
     @Override
     public PoetsResponseDto getPoets() {
-        return poetryApi.getPoets();
+        counter.increment();
+        PoetsResponseDto response = timer.record(() -> poetryApi.getPoets());
+        return response;
     }
 
     @Override
